614e473f3b61bc7fb8a61145aa977e2bdc164c83
SINGH-SAHAB <37237571+SINGH-SAHAB@users.noreply.github.com>
2024-04-08 12:22:45 +0530

    Add files via upload

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,26 +7,46 @@ import Courses from "./components/Route/Courses";
 import Reviews from "./components/Route/Reviews";
 import FAQ from "./components/FAQ/FAQ";
 import Footer from "./components/Footer";
-import dynamic from "next/dynamic";
+import Home from "./components/Route/homelogin";
+import { useLoadUserQuery } from "@/redux/features/api/apiSlice";
 
-// import Attendance from "./components/Route/attandance";
-// import Quiz from "./components/Quiz/Quizecomp"
+
+
 
 interface Props {}
 
+
+
+
+
+
 const Page: FC<Props> = (props) => {
+ 
   const [open, setOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(0);
   const [route, setRoute] = useState("Login");
   
+//const { data } = useSession();
+  const{ data:userData} = useLoadUserQuery(undefined,{});
+  // State to store login status
+
+
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+
+    useEffect(()=>{
+      setIsLoggedIn(!!userData);
+     // alert(!!userData);
+    },[userData]);
+
 
   return (
     <div>
-      <Heading
+     {/* <Heading
         title="ELearning"
         description="ELearning is a platform for students to learn and get help from teachers"
         keywords="Prograaming,MERN,Redux,Machine Learning"
-      />
+  />*/}
       <Header
         open={open}
         setOpen={setOpen}
@@ -34,17 +54,31 @@ const Page: FC<Props> = (props) => {
         setRoute={setRoute}
         route={route}
       />
-      <Hero />
+  
+      {/* Conditionally render either the Hero component or Home component */}
+      {isLoggedIn ? (
+          <>
+            <Home /> 
+            {/*<Courses />
+            <Reviews />
+      <FAQ />*/}
+            {console.log("User is logged in")} {/* Log statement */}
+          </>
+          ) : (
+          <>
+            <Hero />
+            {/*<Courses />*/}
+            {console.log("User is not logged in")} {/* Log statement */}
+          </>
+        )}
+
+
       {/* <Attendance /> */}
-      <Courses />
-      <Reviews />
-      <FAQ />
+      
       {/* <Quiz/> */}
-      <Footer />
+       <Footer />
     </div>
   );
 };
 
-export default dynamic (() => Promise.resolve(Page), {ssr: false})
-
-//export default Page;
+export default Page;
